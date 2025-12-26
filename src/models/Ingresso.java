9a2e2c8fb1cc3bb18fd7568973092d5e8abdba5b
José Eudes <joseeudes969@gmail.com>
2025-12-26 18:24:05 -0300

    ingresso geral

--- src/models/Ingresso.java
+++ src/models/Ingresso.java
@@ -2,5 +2,31 @@ package models;
 
 // valor, nome do filme e informar se é dublado ou legendado
 public class Ingresso {
+    private double valorDoIngresso;
+    private String nomeDoFilme;
+    private boolean isDublado;
 
+    public double getValorDoIngresso() {
+        return valorDoIngresso;
+    }
+
+    public void setValorDoIngresso(double valorDoIngresso) {
+        this.valorDoIngresso = valorDoIngresso;
+    }
+
+    public String getNomeDoFilme() {
+        return nomeDoFilme;
+    }
+
+    public void setNomeDoFilme(String nomeDoFilme) {
+        this.nomeDoFilme = nomeDoFilme;
+    }
+
+    public boolean isDublado() {
+        return isDublado;
+    }
+
+    public void setDublado(boolean dublado) {
+        isDublado = dublado;
+    }
 }
